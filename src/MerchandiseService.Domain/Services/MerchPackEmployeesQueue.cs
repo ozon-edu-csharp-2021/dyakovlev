1d876eb1e5c1edba53866093ca98fc790e836372
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-11-09 18:46:47 +0300

    Domain services

---
+++ src/MerchandiseService.Domain/Services/MerchPackEmployeesQueue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MerchandiseService.Domain.AggregationModels.Employee;
+using MerchandiseService.Domain.AggregationModels.MerchPack;
+
+namespace MerchandiseService.Domain.Services
+{
+    public class MerchPackEmployeesQueue : IMerchPackEmployeesQueue
+    {
+        private Dictionary<MerchPack, Queue<Employee>> _employeesQueue = new();
+        
+        public void Enqueue(MerchPack merchPack, Employee employee)
+        {
+            if (!_employeesQueue.ContainsKey(merchPack))
+            {
+                _employeesQueue[merchPack] = new Queue<Employee>();
+            }
+            _employeesQueue[merchPack].Enqueue(employee);
+        }
+
+        public void DequeueEmployee(MerchPack merchPack)
+        {
+            var employee = _employeesQueue[merchPack].Dequeue();
+            employee.AcceptMerchPack(merchPack);
+        }
+    }
+}
