4209f8acdcce7a8ab465f047a28c2fde01f81257
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-11-09 18:47:00 +0300

    Domain model contracts

---
+++ src/MerchandiseService.Domain/Contracts/IMerchPackBundler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MerchandiseService.Domain.AggregationModels.MerchPack;
+
+namespace MerchandiseService.Domain.Services
+{
+    public interface IMerchPackBundler
+    {
+        Task<bool> CanBundle(MerchPack merchPack, CancellationToken ct);
+        Task ReserveMerchPackItems(MerchPack merchPack, CancellationToken ct);
+    }
+}
---
+++ src/MerchandiseService.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MerchandiseService.Domain.Services
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChanges(CancellationToken ct);
+    }
+}
