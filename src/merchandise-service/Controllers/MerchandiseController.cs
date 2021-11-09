bced6ccf10108d82855a90db8c9f3509bb756f39
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-11-09 18:48:11 +0300

    Controller & http models changed

--- src/merchandise-service/Controllers/MerchandiseController.cs
+++ src/merchandise-service/Controllers/MerchandiseController.cs
@@ -1,7 +1,15 @@
-﻿using System;
-using MerchandiseService.HttpModels;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using MerchandiseService.Domain.AggregationModels.Employee;
+using MerchandiseService.Domain.Services;
+using MerchandiseService.Infrastructure.Commands;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeMerchHistoryRequest = MerchandiseService.Infrastructure.Commands.EmployeeMerchHistoryRequest;
+using EmployeeMerchHistoryResponse = MerchandiseService.HttpModels.EmployeeMerchHistoryResponse;
+using MerchPack = MerchandiseService.Domain.AggregationModels.MerchPack.MerchPack;
 
 namespace MerchandiseService
 {
@@ -10,19 +18,54 @@ namespace MerchandiseService
     [GlobalExceptionFilter]
     public class MerchandiseController : Controller
     {
-        [Route("employee/{employeeId}/merch/{merchType}")]
-        [ProducesResponseType(typeof(MerchResponse), StatusCodes.Status200OK)]
-        [HttpGet]
-        public ActionResult<MerchResponse> RequestMerch(long employeeId, MerchType merchType)
+        private readonly IRepository<Employee> _employees;
+        private readonly IRepository<MerchPack> _merchPacks;
+        private readonly IMediator _mediator;
+
+        public MerchandiseController(IRepository<Employee> employees, IMediator mediator, IRepository<MerchPack> merchPacks)
         {
-            throw new NotImplementedException();
+            _employees = employees;
+            _mediator = mediator;
+            _merchPacks = merchPacks;
         }
-        
+
+        [Route("employee/{employeeId}/merch/{merchPackId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<ActionResult> RequestMerch(int employeeId, int merchPackId)
+        {
+            var employee = await _employees.GetById(employeeId);
+            var merchPack = await _merchPacks.GetById(merchPackId);
+            await _mediator.Send(new MerchRequestCommand
+            {
+                Employee = employee,
+                RequestedPack = merchPack
+            });
+            return Ok();
+        }
+
         [Route("employee/{employeeId}/merch-history")]
         [HttpGet]
-        public ActionResult<EmployeeMerchHistoryResponse> GetEmployeeMerchHistory(long employeeId)
+        public async Task<ActionResult<EmployeeMerchHistoryResponse>> GetEmployeeMerchHistory(int employeeId)
         {
-            throw new NotImplementedException();
+            var employee = await _employees.GetById(employeeId);
+            var merchHistoryResponse = await _mediator.Send(new EmployeeMerchHistoryRequest
+            {
+                Employee = employee,
+            });
+            return Ok(MapToHttpModel(merchHistoryResponse));
+
+            EmployeeMerchHistoryResponse MapToHttpModel(
+                Infrastructure.Commands.EmployeeMerchHistoryResponse source)
+            {
+                return new EmployeeMerchHistoryResponse
+                {
+                    MerchPacks = source.MerchPacks.Select(merchPack => new HttpModels.MerchPack
+                    {
+                        MerchItems = merchPack.MerchPackType.Items.Select(merchItem => new HttpModels.MerchItem())
+                    })
+                };
+            }
         }
     }
 }
