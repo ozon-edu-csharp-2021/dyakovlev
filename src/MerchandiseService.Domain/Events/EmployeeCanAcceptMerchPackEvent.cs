e790e3d5f6ed6e51e263973a87429602d5390959
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-11-09 18:46:35 +0300

    Domain events & exceptions

---
+++ src/MerchandiseService.Domain/Events/EmployeeCanAcceptMerchPackEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using MerchandiseService.Domain.AggregationModels.Employee;
+using MerchandiseService.Domain.AggregationModels.MerchPack;
+
+namespace MerchandiseService.Domain.Events
+{
+    public class EmployeeCanAcceptMerchPackEvent : INotification
+    {
+        public MerchPack MerchPack { get; init; }
+        public Employee Employee { get; init; }
+    }
+}
